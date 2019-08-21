31c621250182e2df492b1c7a70ef36bb48d2b66f
sprogslegs <sprogslegs@users.noreply.github.com>
2019-08-21 13:44:26 +0100

    Added unittest block (incomplete)

--- rock_paper.py
+++ rock_paper.py
@@ -1,40 +1,41 @@
 import unittest
 from random import choice
 
+class RockPaperScissors():
 
+    outcomes_list = ['rock', 'scissors', 'paper']    
 
-outcomes_list = ['rock', 'scissors', 'paper']    
+    outcomes_dict = {
+        'rock-scissors': 'Rock blunt scissors!',
+        'scissors-paper': 'Scissors cut paper!',
+        'paper-rock': 'Paper covers rock'
+    }
 
-outcomes_dict = {
-    'rock-scissors': 'Rock blunt scissors!',
-    'scissors-paper': 'Scissors cut paper!',
-    'paper-rock': 'Paper covers rock!'
-}
+    # generate generate outcome for hand
 
-def generate_hand(outcomes_list):
-    hand = choice(outcomes_list)
-    return hand
+    def generate_hand(outcomes_list):
+        hand = choice(outcomes_list)
+        print(hand)
+        return hand
 
-def play(hand1, hand2, outcomes_dict):
-    
-    print('hand1:', hand1)
-    print('hand12:', hand2)
+    def play(hand1, hand2):
 
-    if hand1 == 'scissors' and hand2 == 'rock' or hand1 == 'rock' and hand2 == 'scissors':
-        outcome = outcomes_dict['rock-scissors']
-    elif hand1 == 'scissors' and hand2 == 'paper' or hand1 == 'paper' and hand2 == 'scissors':
-        outcome = outcomes_dict['scissors-paper']
-    elif hand1 == 'rock' and hand2 == 'paper' or hand1 == 'paper' and hand2 == 'rock':
-        outcome = outcomes_dict['paper-rock']
-    elif hand1 == hand2:
-        outcome = "DRAW!"
-    else:
-        outcome = "WTF??"
-        
-    print(outcome)
-    return outcome
+        if hand1 == 'scissors' and hand2 == 'rock' or hand1 == 'rock' and hand2 == 'scissors':
+            action = outcomes_dict['rock-scissors']
+        elif hand1 == 'scissors' and hand2 == 'paper' or hand1 == 'paper' and hand2 == 'scissors':
+            outcome = outcomes_dict['scissors-paper']
+        elif hand1 == 'rock' and hand2 == 'paper' or hand1 == 'paper' and hand2 == 'rock':
+            outcome = outcomes_dict['paper-rock']
+        elif hand1 == hand2:
+            outcome = "DRAW!"
+            
+        print(outcome)
+        return outcome
 
-hand1 = generate_hand(outcomes_list)
-hand2 = generate_hand(outcomes_list)
+# tests https://docs.python.org/3/library/unittest.html
+class RockPaperTests(unittest.TestCase):
+    def scissors_cut_paper(self):
+        self.assertEqual(1, 1, 'Paper covers rock')
 
-play(hand1, hand2, outcomes_dict)
+if __name__ == '__main__':
+    unittest.main()
