8bb78307f163a82a2299e9f2075974b039014ef0
sprogslegs <sprogslegs@users.noreply.github.com>
2019-09-01 17:57:43 +0100

    Bug fix for refactor to classes

--- rock_paper.py
+++ rock_paper.py
@@ -3,29 +3,30 @@ from random import choice
 
 class RockPaperScissors():
 
-    outcomes_list = ['rock', 'scissors', 'paper']    
+    def __init__(self):
+        self.outcomes_list = ['rock', 'scissors', 'paper']    
 
-    outcomes_dict = {
-        'rock-scissors': 'Rock blunt scissors!',
-        'scissors-paper': 'Scissors cut paper!',
-        'paper-rock': 'Paper covers rock'
-    }
+        self.outcomes_dict = {
+            'rock-scissors': 'Rock blunt scissors!',
+            'scissors-paper': 'Scissors cut paper!',
+            'paper-rock': 'Paper covers rock'
+        }
 
     # generate generate outcome for hand
 
-    def generate_hand(outcomes_list):
+    def generate_hand(self, outcomes_list):
         hand = choice(outcomes_list)
         print(hand)
         return hand
 
-    def play(hand1, hand2):
+    def play(self, hand1, hand2):
 
         if hand1 == 'scissors' and hand2 == 'rock' or hand1 == 'rock' and hand2 == 'scissors':
-            action = outcomes_dict['rock-scissors']
+            outcome = self.outcomes_dict['rock-scissors']
         elif hand1 == 'scissors' and hand2 == 'paper' or hand1 == 'paper' and hand2 == 'scissors':
-            outcome = outcomes_dict['scissors-paper']
+            outcome = self.outcomes_dict['scissors-paper']
         elif hand1 == 'rock' and hand2 == 'paper' or hand1 == 'paper' and hand2 == 'rock':
-            outcome = outcomes_dict['paper-rock']
+            outcome = self.outcomes_dict['paper-rock']
         elif hand1 == hand2:
             outcome = "DRAW!"
             
@@ -34,8 +35,10 @@ class RockPaperScissors():
 
 # tests https://docs.python.org/3/library/unittest.html
 class RockPaperTests(unittest.TestCase):
-    def scissors_cut_paper(self):
-        self.assertEqual(1, 1, 'Paper covers rock')
+    def test_scissors_cut_paper(self):
+        self.assertEqual(game.play('scissors','paper'), 'Scissors cut paper!')
 
+game = RockPaperScissors()
 if __name__ == '__main__':
     unittest.main()
+
