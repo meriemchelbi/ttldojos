1b40d780b245c8566a791a9422ccf7dc1fb1af44
sprogslegs <sprogslegs@users.noreply.github.com>
2019-09-19 17:30:16 +0100

    Fixed build, added basic Runtomato logic & test, started on timetest

--- 18SepDojo/Pomodoro.cs
+++ 18SepDojo/Pomodoro.cs
@@ -1,12 +1,27 @@
 using System;
+using System.Timers;
 
 namespace _SepPomodoro
 {
     public class Pomodoro
     {
-        public void Test1()
-        {
 
+        public int _tomatoCount;
+        public int _tomatoCycleLength;
+        // private static System.Timers.Timer myTimer;
+
+        public Pomodoro(int cycleLength)
+        {
+            _tomatoCount = 0;
+            _tomatoCycleLength = cycleLength;
+        }
+
+        public void RunTomato()
+        {
+            Timer timer = new Timer(_tomatoCycleLength);
+            timer.Start();
+            _tomatoCount++;
+            timer.Stop();
         }
     }
 }
--- 18SepDojo/PomodoroTests.cs
+++ 18SepDojo/PomodoroTests.cs
@@ -1,43 +1,78 @@
 using System;
+using System.Diagnostics;
 using Xunit;
 
 namespace _SepPomodoro
 {
     public class PomodoroTests
     {
-        // Instantiate a Pomodoro object
+        Pomodoro pomodoro = new Pomodoro(5);
 
-        // Add method to start a stopwatch?
-        // variable to log where in cycle? e.g. Cycle1, Pomodoro 3, short break 1, long break.
+        // Add test for anything public
 
-        
-        [Theory]        
-        public void UninterruptedPomodoroTimerAddsTomato()
+            [Fact]
+        public void TestTomatoCountInstantiated
         {
-            // test completed pomodoro timer (25 mins) adds a tomato. 
-            // Check new tomato count based on count at start of cycle
 
         }
 
-        [Theory]
-        public void ShortBreakTriggeredAfterUninterruptedPomodoro()
+        public void TestTomatoCycleLengthInstantiated
+         {
+            
+         }                      
+            
+        // variable to log where in cycle? e.g. Cycle1, Pomodoro 3, short break 1, long break.
+        
+        [Fact]        
+        public void UninterruptedPomodoroTimerAddsTomato()
         {
-            // Test 5 min break is triggered after first, second and third pomodoro cycle
+            
+            // test completed pomodoro timer adds a tomato.
+            pomodoro.RunTomato();
+            pomodoro.RunTomato();
+            pomodoro.RunTomato();
+            // Check new tomato count based on count at start of cycle
+            Assert.Equal(3, pomodoro._tomatoCount);
 
         }
 
         [Fact]
-        public void LongBreakTriggeredAfterFourthUninterruptedPomodoro()
+        public void TwoPomodorosLastExpectedTime()
         {
-            // Test 30 min break is triggered after fourth cycle
+            var stopwatch = new Stopwatch();
+            
+            stopwatch.Start();
+            pomodoro.RunTomato();
+            pomodoro.RunTomato();
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            Assert.Equal(2, pomodoro._tomatoCount);
 
         }
 
-        [Theory]
-        public void CheckTimeElapsedMatchesNumberOfTomatoes()
-        {
-            // Stop time at specific point and check correct number of tomatoes accumulated
-
-        }
     }
+
 }
+
+
+//         [Theory]
+//         public void ShortBreakTriggeredAfterUninterruptedPomodoro()
+//         {
+//             // Test 5 min break is triggered after first, second and third pomodoro cycle
+
+//         }
+
+//         [Fact]
+//         public void LongBreakTriggeredAfterFourthUninterruptedPomodoro()
+//         {
+//             // Test 30 min break is triggered after fourth cycle
+
+//         }
+
+//         [Theory]
+//         public void CheckTimeElapsedMatchesNumberOfTomatoes()
+//         {
+//             // Stop time at specific point and check correct number of tomatoes accumulated
+
+//         }
