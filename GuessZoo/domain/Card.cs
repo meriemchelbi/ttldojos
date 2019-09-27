39ab1ebbd2c5817b138a97c3bf21e4f849e30a9a
sprogslegs <sprogslegs@users.noreply.github.com>
2019-09-27 09:21:40 +0100

    Added GuessZoo implementation from dojo

---
+++ GuessZoo/domain/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessZoo.domain
+{
+    public class Card
+    {
+        public string Colour { get; set; }
+        public string Animal { get; set; }
+        public string Adjective { get; set; }
+    }
+}
---
+++ GuessZoo/service/ManagementSvc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using GuessZoo.domain;
+
+namespace GuessZoo.service
+{
+    public class ManagementSvc
+    {
+        private readonly ListLoaderSvc _listLoaderSvc;
+
+        public ManagementSvc()
+        {
+            _listLoaderSvc = new ListLoaderSvc();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("GuessZoo?");
+            List<Card> allCards = _listLoaderSvc.GetCards();
+            Card selected = PickRandomCard(allCards);
+        }
+
+        public Card PickRandomCard(List<Card> cards)
+        {
+            Random r = new Random();
+            var randInt = r.Next(0, cards.Count);
+            return cards[randInt];
+        }
+    }
+}
