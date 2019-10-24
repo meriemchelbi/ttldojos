5839084f587960d72b2d3e1bbcff8faa5100b9ce
sprogslegs <sprogslegs@users.noreply.github.com>
2019-10-24 14:28:06 +0100

    Added LCDDigit class & start of unit tests

---
+++ LCD Digits/LCDDigitsProgram/LCDDigits/domain/LCDDigit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCDDigits.domain
+{
+    public class LCDDigit
+    {
+        private int index;
+        public LCDDigit(int index)
+        {
+            this.index = index;
+
+        }
+
+        public string GetRepresentation()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ LCD Digits/LCDDigitsTest/LCDTests/TestLCDDigit.cs
@@ -0,0 +1,24 @@
+﻿using LCDDigits.domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LCDTests
+{
+    public class TestLCDDigit
+    {   
+        [Fact]
+        public void ShouldRepresentZero()
+        {
+            var lcdDigit = new LCDDigit(0);
+
+            var representation = lcdDigit.GetRepresentation();
+
+
+
+        }
+
+
+    }
+}
