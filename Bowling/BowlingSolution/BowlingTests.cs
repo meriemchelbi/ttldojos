b6477d105ef2932632509caf3b38c0061ebf056f
sprogslegs <sprogslegs@users.noreply.github.com>
2019-10-24 10:37:28 +0100

    Added new test project

--- Bowling/BowlingSolution/BowlingTests.cs
+++ Bowling/BowlingSolution/BowlingTests.cs
@@ -14,24 +14,7 @@ namespace BowlingSolution
         [InlineData("X X X X X X X X X X X X", "555555555555", "555555555555")]
         public void TestLoadGameScores(string scoresString, string round1Scores, string round2Scores)
         {
-            var bowling = new BowlingGame(scoresString);
-            bowling.LoadGameScores();
-
-            List<Frame> scores = bowling.Scores;
-
-            for (int i = 0; i < scores.Count; i++)
-            {
-                var frame = scores[i];
-
-                var round1Actual = frame.Round1;
-                var round1Expected = int.Parse(round1Scores[i] + "");
-
-                var round2Actual = frame.Round2;
-                var round2Expected = int.Parse(round2Scores[i] + "");
-
-                Assert.Equal(round1Expected, round1Actual);
-                Assert.Equal(round2Expected, round2Actual);
-            }            
+            Assert.Equal(scoresString, round1Scores);
         }
 
         [Fact]
---
+++ DojoTests/BowlingTests/BowlingTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace DojoTests.BowlingTests
+{
+    public class BowlingTests
+    {
+        [Theory]
+        [InlineData("X X X X X X X X X X X X", "555555555555", "555555555555")]
+        public void TestLoadGameScores(string scoresString, string round1Scores, string round2Scores)
+        {
+            var bowling = new BowlingGame(scoresString);
+            bowling.LoadGameScores();
+
+            List<Frame> scores = bowling.Scores;
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                var frame = scores[i];
+
+                var round1Actual = frame.Round1;
+                var round1Expected = int.Parse(round1Scores[i] + "");
+
+                var round2Actual = frame.Round2;
+                var round2Expected = int.Parse(round2Scores[i] + "");
+
+                Assert.Equal(round1Expected, round1Actual);
+                Assert.Equal(round2Expected, round2Actual);
+            }
+        }
+
+        [Fact]
+        public void TestTotalScoreComputed()
+        {
+            // Given collection of frames
+            // When total score computed
+            // Then score matches expected value
+        }
+
+    }
+}
