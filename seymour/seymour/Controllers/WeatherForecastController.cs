d3002924ec94d8a703a07dfc51b4ccc6725a8764
meriem.chelbi <meriem.chelbi@thetrainline.com>
2019-11-29 15:10:35 +0000

    Added Seymour

---
+++ seymour/seymour/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using seymour.Services;
+
+namespace seymour.Controllers
+{
+    [ApiController]
+    [Route("weatherforecast")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IWeatherService _weatherService;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherForecastController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet()]
+        public IEnumerable<WeatherForecast> Get(string message)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            var bob = true;
+        }
+    }
+}
---
+++ seymour/seymour/Services/IWeatherService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace seymour.Services
+{
+    public interface IWeatherService
+    {
+        string GetMessage();
+    }
+
+    public class WeatherService : IWeatherService
+
+    {
+        public string GetMessage()
+        {
+            return "skjdksjds";
+        }
+    }
+
+    public class PremiumsWervice : IWeatherService
+    {
+        public string GetMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
